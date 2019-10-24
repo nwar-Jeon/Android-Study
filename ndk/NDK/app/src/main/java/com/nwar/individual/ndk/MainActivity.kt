51559f9cece63b1ff2adff6e6f299f3c2e7a04cf
nwar-Jeon <nwar0825@naver.com>
2019-10-24 19:19:01 +0900

    ndk 공부 시작

--- ndk/NDK/app/src/main/java/com/nwar/individual/ndk/MainActivity.kt
+++ ndk/NDK/app/src/main/java/com/nwar/individual/ndk/MainActivity.kt
@@ -1,19 +1,12 @@
-package com.nwar.individual.eyetrackingar
+package com.nwar.individual.ndk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.ar.core.
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        if()
     }
 }
